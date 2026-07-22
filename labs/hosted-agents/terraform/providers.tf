31aa48042d2b19178376e0437ad42e26f6073f6d
Michał Smyk <michal@smyk.it>
2026-07-22 20:52:30 +0200

    Clean-up

--- labs/hosted-agents/terraform/providers.tf
+++ labs/hosted-agents/terraform/providers.tf
@@ -10,9 +10,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-
+  resource_provider_registrations = "core"
   use_oidc        = true
-  subscription_id = var.azure_subscription_id
-  tenant_id       = var.azure_tenant_id
-  client_id       = var.azure_client_id
+
 }
--- labs/hosted-agents/terraform/variables.tf
+++ labs/hosted-agents/terraform/variables.tf
@@ -8,21 +8,3 @@ variable "environment" {
     error_message = "Environment must be one of: dev, staging, prod"
   }
 }
-
-variable "azure_subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "azure_tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "azure_client_id" {
-  description = "Azure Client ID (Application ID)"
-  type        = string
-  sensitive   = true
-}
